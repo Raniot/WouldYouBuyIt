343710d80a975a6d3f4bf963353d902d0f470223
Nicklas <nnicklas92@gmail.com>
2021-07-25 22:32:47 +0200

    Image in fullscreen

--- lib/widgets/imagePanel.dart
+++ lib/widgets/imagePanel.dart
@@ -20,11 +20,48 @@ class ImagePanel extends StatelessWidget {
       width: 300,
       child: PhotoViewGallery.builder(
         itemCount: images.length, 
+        pageController: PageController(initialPage: 0),
         scrollPhysics: const BouncingScrollPhysics(),
         builder: (BuildContext context, int index) {
           return PhotoViewGalleryPageOptions(
-            imageProvider: NetworkImage(images[index].largeImageUrl),
+            imageProvider: NetworkImage(images[index].thumbnailUrl),
             initialScale: PhotoViewComputedScale.contained,
+            onTapUp: (context, details, value) {
+              Navigator.push(
+                context, 
+                MaterialPageRoute(
+                  builder: (context) {
+                    return Container(
+                      child: PhotoViewGallery.builder(
+                          itemCount: images.length,
+                          pageController: PageController(initialPage: index),
+                          scrollPhysics: const BouncingScrollPhysics(),
+                          builder: (context, index) {
+                            return PhotoViewGalleryPageOptions(
+                              imageProvider: NetworkImage(images[index].largeImageUrl),
+                              initialScale: PhotoViewComputedScale.contained,
+                              onTapUp: (context, details, value) {
+                                Navigator.pop(context);
+                              }
+                            );
+                          },
+                          loadingBuilder: (context, event) => Center(
+                            child: Container(
+                              child: CircularProgressIndicator(
+                                value: event == null
+                                  ? 0
+                                  : event.cumulativeBytesLoaded / int.parse(event.expectedTotalBytes.toString())
+                              ),
+                            ),
+                          ),
+                          backgroundDecoration: BoxDecoration(
+                            color: Colors.transparent
+                          ),
+                        ),
+                    );
+                  })
+              );
+            }
           );
         },
         loadingBuilder: (context, event) => Center(
