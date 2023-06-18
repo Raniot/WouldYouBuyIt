a92c80d4ee122c274044bc196f681340c3231730
Nicklas <nnicklas92@gmail.com>
2023-06-18 13:03:57 +0200

    Removed unessasary complexity

--- lib/models/imageData.dart
+++ lib/models/imageData.dart
@@ -1,5 +1,3 @@
-import 'package:would_you_buy_it/rest/json.dart';
-
 class ImageData {
   String largeImageUrl;
   String lowResTabletImageUrl;
@@ -19,15 +17,6 @@ class ImageData {
       lowResTabletImageUrl: json['lowResTabletImageUrl'],
       thumbnailUrl: json['thumbnailUrl'],
       type: json['type'],
-    );
-  }
-
-  factory ImageData.fromJsonObject(JsonObject json) {
-    return ImageData(
-      largeImageUrl: json.mandatoryString('largeImageUrl'), 
-      lowResTabletImageUrl: json.mandatoryString('lowResTabletImageUrl'), 
-      thumbnailUrl: json.mandatoryString('thumbnailUrl'), 
-      type: json.mandatoryString('type'),
     );
   }
 }
--- lib/services/WouldYouBuyItService.dart
+++ lib/services/WouldYouBuyItService.dart
@@ -1,18 +1,18 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
-import 'package:would_you_buy_it/rest/json.dart';
-import 'package:would_you_buy_it/rest/jsonObjectMapper.dart';
 
 import '../models/house.dart';
 
 Future<House> fetchHouse() async {
-  return await fetch('https://wouldyoubuyitapi.azurewebsites.net/api/house', House.fromJsonObject);
+  return await fetch('https://wouldyoubuyitapi.azurewebsites.net/api/house', House.fromJson);
 }
 
-Future<T> fetch<T>(String uri, T Function(JsonObject json) mapper) async {
+Future<T> fetch<T>(String uri, T Function(Map<String, dynamic> json) mapper) async {
   for(var i = 0; i < 5; i++) {
     final response = await http.get(Uri.parse(uri));
     if (response.statusCode == 200) {
-      return jsonObjectMapper(mapper).map(response);
+      return mapper(jsonDecode(response.body));
     }
   }
 
